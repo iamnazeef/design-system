43b9206fa2ffe88b0b2c2630060b8ef458db16dd
iamnazeef <muhammednazeef6@gmail.com>
2024-05-19 13:30:54 +0530

    add .prettierrc and rearrange packages

---
+++ packages/components/input/index.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './index';
+
+const meta: Meta<typeof Input> = {
+  title: 'Input',
+  component: Input,
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Filled: Story = {
+  args: {},
+};
