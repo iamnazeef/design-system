5688a5befb2263b68a87df0a84c3937c68b069b5
iamnazeef <muhammednazeef6@gmail.com>
2024-05-19 15:35:03 +0530

    add tokens

--- packages/components/input/index.tsx
+++ packages/components/input/index.tsx
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react';
+import { styled } from 'styled-components';
+import reference from '@tokens/reference';
+
+const EvButton = styled('input')(() => ({
+  outline: 'none',
+  backgroundColor: reference.light.entriBlue.default,
+}));
 
 export function Input() {
-    return <input />
+  return <EvButton />;
 }
